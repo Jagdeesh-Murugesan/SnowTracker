ff90aae83d964e9690c12e3009af103b4d02b5dc
Murugesan <jagdeesh.murugesan@accenture.com>
2018-09-26 15:46:33 +0530

    Merged Guhan, Lalitha and Vasagamani code

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
-
 const Alexa = require('ask-sdk-core');
 const snowRestClient = require('./modules/snowHelper');
 const config = require('./config');
@@ -64,8 +63,46 @@ const TicketPortfolioCountIntentHandler = {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'TicketPortfolioCountIntent';
   },
-  handle(handlerInput) {
-    const speechText = 'TicketPortfolioCountIntent';
+  async handle(handlerInput) {
+    let portFolioList = [];
+    let speechText = 'TicketPortfolioCountIntent';
+    let resultCount = 0;
+    let portfolioCount=0;
+    
+    await snowRestClient.getTicketCountByPortfolio().then(function(response){
+        
+        resultCount = response.result.length;
+        response.result.forEach(e => {
+          if (e.category != null && e.category !="") {
+          portFolioList.push(e.category);
+          }
+       });
+       //speechText = portFolioList.length.toString();
+      portfolioCount = portFolioList.length;
+      portFolioList.sort();
+
+      let current = null;
+      let cnt = 0;
+      for (let i = 0; i < portfolioCount; i++) {
+          if (portFolioList[i] != current) {
+              if (cnt > 0) {
+                  //document.write(current + ' comes --> ' + cnt + ' times<br>');
+                  speechText = speechText + `The portfolio ${current} has ${cnt} tickets`;
+              }
+              current = portFolioList[i];
+              cnt = 1;
+          } else {
+              cnt++;
+          }
+      }
+    if (cnt > 0) {
+       // document.write(current + ' comes --> ' + cnt + ' times');
+        speechText = speechText + `The portfolio ${current} has ${cnt} tickets`;
+    }        
+       
+    },function(err){
+      console.log(" Err occured "+err);
+    });
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -78,8 +115,47 @@ const TicketVendorCountIntentHandler = {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'TicketVendorCountIntent';
   },
-  handle(handlerInput) {
-    const speechText = 'TicketVendorCountIntent';
+  async handle(handlerInput) {
+    let vendorList = [];
+    let speechText = '';
+    let resultCount = 0;
+    let vendorCount=0;
+    let serviceDeskCnt = 0;
+    let networkCnt = 0;
+    let softwareCnt = 0;
+    let databaseCnt = 0;
+    
+    await snowRestClient.getTicketCountByVendor().then(function(response){
+        
+        resultCount = response.result.length;
+        response.result.forEach(e => {
+          if (e.assignment_group.value != null && e.assignment_group.value !="") {
+          vendorList.push(e.assignment_group.value);
+          }
+       });
+       //speechText = portFolioList.length.toString();
+      vendorCount = vendorList.length;
+      vendorList.sort();
+
+      let current = null;
+      let cnt = 0;
+      for (let i = 0; i < vendorCount; i++) {
+        
+          if (vendorList[i] == "d625dccec0a8016700a222a0f7900d06") {
+            serviceDeskCnt++;
+          } else if (vendorList[i] == "287ebd7da9fe198100f92cc8d1d2154e") {
+            networkCnt++;
+          }else if (vendorList[i] == "287ee6fea9fe198100ada7950d0b1b73") {
+            databaseCnt++;
+          }else if (vendorList[i] == "8a4dde73c6112278017a6a4baf547aa7") {
+            softwareCnt++;
+          }
+      }
+        speechText = `There are ${serviceDeskCnt} tickets in  service desk  ${networkCnt} in network ${databaseCnt} in database ${softwareCnt} in software as part of assignment group`;
+      
+    },function(err){
+      console.log(" Err occured "+err);
+    });
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -92,9 +168,34 @@ const TicketAppCountIntentHandler = {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'TicketAppCountIntent';
   },
-  handle(handlerInput) {
-    const speechText = 'TicketAppCountIntent';
+  async handle(handlerInput) {
+    let speechText = 'TicketAppCountIntent';
+    let activeCnt = 0;
+    let emailCnt = 0;
+    let osCnt = 0;
+    let otherAppCnt = 0;
+    
+    await snowRestClient.getTicketCountByApplication().then(function(response){
+        
+        activeCnt = response.result.length;
+        response.result.forEach(e => {
+          if(e.subcategory == 'email'){
+            emailCnt++;
+          }
+          else if(e.subcategory == 'os'){
+            osCnt++;
+          }
+          else{
+            otherAppCnt++;
+          }
+        });
+        speechText = `There are ${emailCnt} open tickets in Email Application and ${osCnt} open tickets in OS Application,
+        under software category.`; 
 
+    },function(err){
+      console.log(" Err occured "+err);
+    });
+    
     return handlerInput.responseBuilder
       .speak(speechText)
       .getResponse();
--- lambda/custom/modules/snowHelper.js
+++ lambda/custom/modules/snowHelper.js
@@ -12,9 +12,51 @@ snowRestClient.getTicketCountByUrgency = function(){
         headers: {
           "Authorization": "Basic " + (new Buffer(config.username + ":" + config.password)).toString("base64"),
           "Accept": "application/json"
-        }
+        } 
       });  
     
+}
+
+snowRestClient.getTicketCountByPortfolio = function(){
+
+  return req({
+      url: "https://dev52396.service-now.com/api/now/table/incident?sysparm_query=active=true&sysparm_fields=number,category",
+      method: "GET",
+      json: true,
+      headers: {
+        "Authorization": "Basic " + (new Buffer(config.username + ":" + config.password)).toString("base64"),
+        "Accept": "application/json"
+      }
+    });  
+  
+}
+
+snowRestClient.getTicketCountByApplication = function(){
+
+  return req({
+      url: "https://dev52396.service-now.com/api/now/table/incident?sysparm_query=active=true&category=software&sysparm_fields=number,subcategory",
+      method: "GET",
+      json: true,
+      headers: {
+        "Authorization": "Basic " + (new Buffer(config.username + ":" + config.password)).toString("base64"),
+        "Accept": "application/json"
+      }
+    });  
+  
+}
+
+snowRestClient.getTicketCountByVendor = function(){
+
+  return req({
+      url: "https://dev52396.service-now.com/api/now/table/incident?sysparm_query=active=true&sysparm_fields=number,assignment_group",
+      method: "GET",
+      json: true,
+      headers: {
+        "Authorization": "Basic " + (new Buffer(config.username + ":" + config.password)).toString("base64"),
+        "Accept": "application/json"
+      }
+    });  
+  
 }
 
 module.exports = snowRestClient;
